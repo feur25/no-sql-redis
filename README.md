16da2e62d1c92e084a8c237a6abfb1551a4f9f2a
feur25 <feur09@gmail.com>
2024-05-10 10:15:27 +0200

    flask endpoint

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 1. Télécharger le projet `$ git clone hhttps://github.com/feur25/no-sql-redis`
 2. `docker-compose build`
 3. `docker-compose up` ou `docker-compose up -d`
+
+
+docker exec -it docker-redis-cluster-redis1-1 redis-cli -c -h 173.17.0.2 -p 7000
+
+
+SET employe:1 '{"nom": "John Doe", "poste": "Développeur", "age": 30}'
+
+GET employe:1
---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, jsonify
+from rediscluster import RedisCluster
+
+# Création de l'application Flask
+app = Flask(__name__)
+
+# Configuration des nœuds Redis dans le cluster
+startup_nodes = [
+    {"host": "173.17.0.2", "port": "7000"},
+    {"host": "173.17.0.3", "port": "7001"},
+    {"host": "173.17.0.4", "port": "7002"}
+]
+
+# Connexion au cluster Redis
+client = RedisCluster(startup_nodes=startup_nodes, decode_responses=True)
+
+# Route pour ajouter un nouvel employé
+@app.route('/employes', methods=['POST'])
+def ajouter_employe():
+    # Récupérer les données JSON de la requête
+    data = request.json
+    nom = data.get('nom')
+    poste = data.get('poste')
+    age = data.get('age')
+
+    # Générer un nouvel ID pour l'employé
+    id_employe = client.incr('employeIdCounter')
+
+    # Enregistrer les informations de l'employé dans Redis
+    employe_key = f'employe:{id_employe}'
+    client.hmset(employe_key, {'nom': nom, 'poste': poste, 'age': age})
+
+    # Réponse JSON
+    response = {
+        'message': 'Nouvel employé ajouté avec succès !',
+        'employeId': id_employe
+    }
+    return jsonify(response), 201
+
+# Point d'entrée de l'application
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=3000)
