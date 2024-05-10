81fa03fc1f380b606220851e841eaa48fbb8da05
feur25 <feur09@gmail.com>
2024-05-10 10:33:14 +0200

    endpoint

--- app.py
+++ app.py
@@ -37,6 +37,28 @@ def ajouter_employe():
     }
     return jsonify(response), 201
 
+# Route pour ajouter un nouvel utilisateur
+@app.route('/utilisateurs', methods=['POST'])
+def ajouter_utilisateur():
+    # Récupérer les données JSON de la requête
+    data = request.json
+    username = data.get('username')
+    email = data.get('email')
+
+    # Générer un nouvel ID pour l'utilisateur
+    id_utilisateur = client.incr('utilisateurIdCounter')
+
+    # Enregistrer les informations de l'utilisateur dans Redis
+    utilisateur_key = f'utilisateur:{id_utilisateur}'
+    client.hmset(utilisateur_key, {'username': username, 'email': email})
+
+    # Réponse JSON
+    response = {
+        'message': 'Nouvel utilisateur ajouté avec succès !',
+        'userId': id_utilisateur
+    }
+    return jsonify(response), 201
+
 # Point d'entrée de l'application
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=3000)
